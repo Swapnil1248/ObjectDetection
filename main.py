ee17c0ffd8be149242efc854ad4cb77f40b0643a
Swapnil Kumar <Swapnil248@users.noreply.github.com>
2017-05-18 09:08:24 -0400

    Update main

--- main.py
+++ main.py
@@ -22,37 +22,28 @@ if __name__ == '__main__':
     # The first step will be creation of the dataset for the Stage 1 of the OCR pipeline from -
     # Dataset containing images with text - The Chars74K dataset
     # Dataset containing random images without text - Kaggle CIFAR-10
-    #make_dataset_Stage1.create_dataset(valid_dir_path, invalid_dir_path)
+    make_dataset_Stage1.create_dataset(valid_dir_path, invalid_dir_path)
 
     # This step will generate the dataset for the Stage 2 of the OCR pipeline from - Chars74K dataset
-    #make_dataset_Stage2.create_dataset(valid_dir_path, csv_path)
+    make_dataset_Stage2.create_dataset(valid_dir_path, csv_path)
 
     # This step will train the model for Stage 2 for the OCR pipeline and generate the best model
-    #sgd_optimization_mnist(dataset='char_num_check.pkl.gz', n_epochs=1000, batch_size= 3000 ,best_model='best_model_char_num_check.pkl', n_output = 2);
+    sgd_optimization_mnist(dataset='char_num_check.pkl.gz', n_epochs=1000, batch_size= 3000 ,best_model='best_model_char_num_check.pkl', n_output = 2);
 
     # This step will train the model for Stage 3 for the OCR pipeline and generate the best model
-    #sgd_optimization_mnist(dataset='char_num_classify.pkl.gz', n_epochs=2000, batch_size= 350, best_model='best_model_char_num_classify.pkl', n_output = 123);
+    sgd_optimization_mnist(dataset='char_num_classify.pkl.gz', n_epochs=2000, batch_size= 350, best_model='best_model_char_num_classify.pkl', n_output = 123);
 
     # Stage 1: Pre process the input image.
-    #image = ProcessImage('hello_world.jpg')
+    image = ProcessImage('hello_world.jpg')
 
     # Stage 1: Pre process the input image to get the contour and image objects.
-    #candidates = image.get_text_candidates()
+    candidates = image.get_text_candidates()
 
     # Stage 2: Text Detection, detects objects containing text within the image.
-    #final_candidates = CheckCharacter().check_character(candidates["flattened"], best_model='best_model_char_num_check.pkl')
-    #final_candidates = candidates["flattened"]
+    final_candidates = CheckCharacter().check_character(candidates["flattened"], best_model='best_model_char_num_check.pkl')
+    final_candidates = candidates["flattened"]
 
     # Stage 3: Character Recognition, Determines the character contained in the image.
-    #PredictCharacter().predict_character(final_candidates, best_model='best_model_char_num_classify.pkl')
+    PredictCharacter().predict_character(final_candidates, best_model='best_model_char_num_classify.pkl')
 
-    print "Classified as: H"
-    print "Classified as: o"
-    print "Classified as: l"
-    print "Classified as: l"
-    print "Classified as: a"
-    print "Classified as: w"
-    print "Classified as: a"
-    print "Classified as: r"
-    print "Classified as: l"
-    print "Classified as: a"
+   
